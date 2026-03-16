49db66506ab1ab21968e37d94f8fcd9cf841dcf9
Pberu <pberu8531@gmail.com>
2026-03-16 21:46:32 +0300

    whatsapp module v2:added provider

--- src/logger/logger.provider.ts
+++ src/logger/logger.provider.ts
@@ -3,6 +3,6 @@ import { logger } from "./winston.logger";
 export const APP_LOGGER = 'APP_LOGGER';
 
 export const loggerProvider = {
-    provide:APP_LOGGER,
-    useValue:logger
+  provide:APP_LOGGER,
+  useValue:logger
 }
