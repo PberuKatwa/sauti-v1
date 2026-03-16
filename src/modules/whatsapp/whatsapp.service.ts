b4e756fe02b122f1502492705b7a053c3ae386c2
Pberu <pberu8531@gmail.com>
2026-03-16 20:15:46 +0300

    whatsapp integration v6:added axios response

--- src/modules/whatsapp/whatsapp.service.ts
+++ src/modules/whatsapp/whatsapp.service.ts
@@ -1,6 +1,8 @@
+import axios, { AxiosError } from "axios";
 import { Inject, Injectable } from "@nestjs/common";
 import { APP_LOGGER } from "../../logger/logger.provider";
 import type { AppLogger } from "../../logger/winston.logger";
+import { WhatsappUnionMessage } from "../../types/whatsapp.base";
 
 @Injectable()
 export class WhatsappService{
@@ -10,8 +12,11 @@ export class WhatsappService{
 
   constructor(@Inject(APP_LOGGER) private readonly logger: AppLogger) { };
 
-  private callApi() {
+  private callApi( recepient:string, data:WhatsappUnionMessage ) {
     try {
+      this.logger.httpreq("Calling whatsapp api", { recipient: recepient, data: data.type });
+
+      const response = ``
 
     } catch (error) {
       throw error;
