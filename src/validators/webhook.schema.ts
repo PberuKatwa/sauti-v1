cc8554f09a7948a139c61a2521315fca930e21a2
Pberu <pberu8531@gmail.com>
2026-03-17 17:50:50 +0300

    Handler validator v2:added zod validator

--- src/validators/webhook.schema.ts
+++ src/validators/webhook.schema.ts
@@ -1,5 +1,43 @@
 import { z } from 'zod';
 
+const StatusesValueSchema = z.object({
+  id: z.string(),
+  status: z.string(),
+  timestamp: z.string(),
+  recepient_id: z.string(),
+});
+
+const ContactsValueSchema = z.object({
+  profile: z.object({
+    name: z.string(),
+  }),
+  wa_id: z.string(),
+});
+
+const InteractiveSchema = z.object({
+  type: z.enum(["button_reply", "list_reply"]).optional(),
+  button_reply: z.object({
+    id: z.string(),
+    title: z.string(),
+  }).optional(),
+  list_reply: z.object({
+    id: z.string(),
+    title: z.string(),
+    description: z.string().optional(),
+  }).optional(),
+});
+
+const IncomingMessagesSchema = z.object({
+  from: z.string(),
+  id: z.string(),
+  timestamp: z.string(),
+  type: z.enum(["text", "interactive"]),
+  text: z.object({
+    body: z.string(),
+  }).optional(),
+  interactive: InteractiveSchema.optional(),
+});
+
 export const WhatsappWebhookSchema = z.object({
   object: z.literal("whatsapp_business_account"),
   entry: z.array(z.object({
@@ -8,15 +46,12 @@ export const WhatsappWebhookSchema = z.object({
       field: z.string(),
       value: z.object({
         messaging_product: z.literal("whatsapp"),
-        messages: z.array(z.object({
-          from: z.string(),
-          id: z.string(),
-          timestamp: z.string(),
-          type: z.enum(["text", "interactive"]),
-          text: z.object({ body: z.string() }).optional(),
-          interactive: z.any().optional(), // You can expand this as needed
-        })).optional(),
+        statuses: z.array(StatusesValueSchema).optional(),
+        contacts: z.array(ContactsValueSchema).optional(),
+        messages: z.array(IncomingMessagesSchema).optional(),
       }).optional(),
     }))
   }))
 });
+
+export type WhatsappWebhookDto = z.infer<typeof WhatsappWebhookSchema>;
