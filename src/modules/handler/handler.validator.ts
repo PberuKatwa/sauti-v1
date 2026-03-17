4d2bd61a0a1aed0976a848e9e457e52ea8a1319f
Pberu <pberu8531@gmail.com>
2026-03-17 18:01:12 +0300

    Handler validator v4:added webhook validator

--- src/modules/handler/handler.validator.ts
+++ src/modules/handler/handler.validator.ts
@@ -1,9 +1,18 @@
 import { Inject } from "@nestjs/common";
 import { APP_LOGGER } from "../../logger/logger.provider";
 import { AppLogger } from "../../logger/winston.logger";
+import { WhatsappWebhook } from "../../types/whatsapp.webhook";
 
 export class HandlerValidator{
 
-  constructor( @Inject(APP_LOGGER) private readonly logger:AppLogger ) { };
+  constructor(@Inject(APP_LOGGER) private readonly logger: AppLogger) { };
+
+  private validateWhatsappWebhook(webhook:WhatsappWebhook) {
+    try {
+
+    } catch (error) {
+      throw error;
+    }
+  }
 
 }
