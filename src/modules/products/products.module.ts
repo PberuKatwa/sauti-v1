d718a923d0a654e2476a8303570bc889051fd039
Pberu <pberu8531@gmail.com>
2026-03-23 16:29:45 +0300

    Products module v5:added handlers

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { WhatsappController } from "../whatsapp/whatsapp.controller";
+import { ProductsHandler } from "./products.handler";
 
 @Module({
   imports: [WhatsappController],
-
+  providers: [ProductsHandler],
+  exports: [ProductsHandler]
 })
 export class ProductsModule { };
