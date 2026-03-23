e478dcf334b7c54631c33a11bcd88dd8f43a3a66
Pberu <pberu8531@gmail.com>
2026-03-23 10:16:29 +0300

    Products module v2:imported whatsapp module

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,6 +1,8 @@
 import { Module } from "@nestjs/common";
+import { WhatsappController } from "../whatsapp/whatsapp.controller";
 
 @Module({
+  imports: [WhatsappController],
 
 })
 export class ProductsModule { };
